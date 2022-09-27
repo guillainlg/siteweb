9f017f6380d7dcf9caac611b5a6b402fa16c7c97
guillainlg <93674296+guillainlg@users.noreply.github.com>
2022-09-27 22:06:50 +0200

    Add files via upload

--- fichier.js
+++ fichier.js
@@ -1,4 +1,4 @@
-var callBackGetSuccess = function(data) {
+var callBackGetSuccess1 = function(data) {
 	console.log("donnees test api", data)
 	var element1 = document.getElementById("f1");
 	var element2 = document.getElementById("f2");
@@ -6,7 +6,7 @@ var callBackGetSuccess = function(data) {
 	var element4 = document.getElementById("f4");
 	var element5 = document.getElementById("f5");
 	var element6 = document.getElementById("f6");
-	element1.innerHTML = "Horaires Depart: Gare Rives de l'Orne ; Arrivee: Campus 2"
+	element1.innerHTML = "Gare Rives -> Campus 2"
 	element2.innerHTML = data.records[0].fields.horaire_depart_theorique;
 	element3.innerHTML = data.records[1].fields.horaire_depart_theorique;
 	element4.innerHTML = data.records[2].fields.horaire_depart_theorique;
@@ -18,9 +18,9 @@ var callBackGetSuccess = function(data) {
 
 
 
-function buttonClickGET() {
+function fct1() {
 	var url = "https://data.twisto.fr/api/records/1.0/search/?dataset=horaires-tr&q=&sort=horaire_depart_theorique&facet=ligne&facet=nom_de_l_arret_stop_name&facet=destination_stop_headsign&facet=etat_de_la_course&refine.ligne=T2&refine.destination_stop_headsign=CAEN+Campus+2&refine.nom_de_l_arret_stop_name=Caen-Gare-Rives+de+l%27Orne-GARE-+RIVES+DE+L%27ORNE"
-	$.get(url, callBackGetSuccess).done(function() {
+	$.get(url, callBackGetSuccess1).done(function() {
 	})
 	.fail(function() {
 		alert( "error" );
@@ -28,3 +28,34 @@ function buttonClickGET() {
 	.always(function() {
 	});
 }
+
+var callBackGetSuccess2 = function(data) {
+	console.log("donnees test api", data)
+	var element1 = document.getElementById("f7");
+	var element2 = document.getElementById("f8");
+	var element3 = document.getElementById("f9");
+	var element4 = document.getElementById("f10");
+	var element5 = document.getElementById("f11");	
+	var element6 = document.getElementById("f12");
+	element1.innerHTML = "Campus2 -> Gare Rives"
+	element2.innerHTML = data.records[0].fields.horaire_depart_theorique;
+	element3.innerHTML = data.records[1].fields.horaire_depart_theorique;
+	element4.innerHTML = data.records[2].fields.horaire_depart_theorique;
+	element5.innerHTML = data.records[3].fields.horaire_depart_theorique;
+	element6.innerHTML = data.records[4].fields.horaire_depart_theorique;
+}
+
+
+
+function fct2() {
+	var url = "https://data.twisto.fr/api/records/1.0/search/?dataset=horaires-tr&q=&sort=horaire_depart_theorique&facet=ligne&facet=nom_de_l_arret_stop_name&facet=destination_stop_headsign&facet=etat_de_la_course&refine.ligne=T2&refine.nom_de_l_arret_stop_name=Caen-CAMPUS+2&refine.destination_stop_headsign=CAEN+Presqu%27ile"
+
+	$.get(url, callBackGetSuccess2).done(function() {
+	})
+	.fail(function() {
+		alert( "error" );
+	})
+	.always(function() {
+	});
+}
+
